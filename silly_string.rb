247c4c382c508d607d5d2c6d3d1878fbb628c216
Rachel Pierron <rachelopierron@gmail.com>
2017-06-06 15:24:51 +0000

    ginished silly string lab

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,9 @@
 #code your solutions here
-
-
-
+my_word = "Supercalifragilisticexpialidocious"
+puts my_word.upcase
+shout = "I'M NOT SHOUTING"
+puts shout.downcase
+kk = "Karlie Kloss"
+puts kk.length
+puts "wow " + "mom"
+puts "i really like programming".capitalize
